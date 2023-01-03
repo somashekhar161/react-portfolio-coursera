baeb951e74d1ca3740fd1be1d6ae1fdf82ee63d3
Somashekar B R <sanjayrao161@gmail.com>
2023-01-03 16:40:33 +0530

    Landning page (step 2)done

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -15,22 +15,22 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <Avatar
+      name="I am Pete!"
+      size="2xl"
+      src={"https://i.pravatar.cc/150?img=7"}
+    />
     <VStack spacing={"23px"}>
-      <Avatar
-        name="I am Pete!"
-        size="2xl"
-        src={"https://i.pravatar.cc/150?img=7"}
-      />
-      <Heading as="h3" size="lg">
+      <Heading as="h3" size="md">
         {greeting}
       </Heading>
-      <Heading as="h1" size="4xl" noOfLines={1}>
+      <Heading as="h1" size="2xl" noOfLines={1}>
         {bio1}
       </Heading>
-      <Heading as="h1" size="4xl" noOfLines={1}>
-        {bio2}
-      </Heading>
     </VStack>
+    <Heading as="h1" size="2xl" noOfLines={1}>
+      {bio2}
+    </Heading>
   </FullScreenSection>
 );
 
